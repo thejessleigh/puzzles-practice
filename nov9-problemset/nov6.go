c1c7625c79a273a295d57220b4e7a00be82078cc
thejessleigh <j.l.unrein@gmail.com>
2018-11-09 11:37:38 -0600

    array product

---
+++ nov9-problemset/nov6.go
@@ -0,0 +1,22 @@
+// Calculate the product of all elements in an array
+
+package main
+
+import (
+	"fmt"
+)
+
+func Product(nums []int)(x int) {
+	x = 1
+
+	for _, num := range nums {
+		x *= num
+	}
+	return
+}
+
+
+func main(){
+	fmt.Println(Product([]int{1, 2, 3, 4, 5}))
+	fmt.Println(Product([]int{2, 4, 6, 8, 10}))
+}
